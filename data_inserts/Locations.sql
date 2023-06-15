9130299503e2e4c311e9fd79f5016619e7bce57b
kbtrack94 <kbtrack94@yahoo.com>
2023-06-15 12:02:55 -0400

    commit

---
+++ data_inserts/Locations.sql
@@ -0,0 +1,18 @@
+CREATE TABLE MEET_LOCATION (
+    SEASON INTEGER
+    ,HOST VARCHAR(255)
+    ,ADDRESS VARCHAR(255)
+    ,FIELD_NAME VARCHAR(255)
+    ,CITY VARCHAR(255)
+    ,STATE VARCHAR(255)
+    ,ZIP_CODE INT
+    );
+
+INSERT INTO MEET_LOCATION (SEASON,HOST,ADDRESS,FIELD_NAME,CITY,STATE,ZIP_CODE) values (2015,'University of Oregon','Hayward Field','1530 Agate Street','Eugene','Oregon',97403)
+INSERT INTO MEET_LOCATION (SEASON,HOST,ADDRESS,FIELD_NAME,CITY,STATE,ZIP_CODE) values (2016,'University of Oregon','Hayward Field','1530 Agate Street','Eugene','Oregon',97403)
+INSERT INTO MEET_LOCATION (SEASON,HOST,ADDRESS,FIELD_NAME,CITY,STATE,ZIP_CODE) values (2017,'University of Oregon','Hayward Field','1530 Agate Street','Eugene','Oregon',97403)
+INSERT INTO MEET_LOCATION (SEASON,HOST,ADDRESS,FIELD_NAME,CITY,STATE,ZIP_CODE) values (2018,'University of Oregon','Hayward Field','1530 Agate Street','Eugene','Oregon',97403)
+INSERT INTO MEET_LOCATION (SEASON,HOST,ADDRESS,FIELD_NAME,CITY,STATE,ZIP_CODE) values (2019,'University of Texas at Austin','Mike A. Myers Stadium and Soccer Field','707 Clyde Littlefield Dr.','Austin','Texas',78712)
+INSERT INTO MEET_LOCATION (SEASON,HOST,ADDRESS,FIELD_NAME,CITY,STATE,ZIP_CODE) values (2021,'University of Oregon','Hayward Field','1530 Agate Street','Eugene','Oregon',97403)
+INSERT INTO MEET_LOCATION (SEASON,HOST,ADDRESS,FIELD_NAME,CITY,STATE,ZIP_CODE) values (2022,'University of Oregon','Hayward Field','1530 Agate Street','Eugene','Oregon',97403)
+INSERT INTO MEET_LOCATION (SEASON,HOST,ADDRESS,FIELD_NAME,CITY,STATE,ZIP_CODE) values (2023,'University of Texas at Austin','Mike A. Myers Stadium and Soccer Field','707 Clyde Littlefield Dr.','Austin','Texas',78712)
